bbf32b02a90c99b2b95ba98a822263caeeebe1bb
Dave Cramer <dave.cramer@credativ.ca>
2014-11-25 16:06:01 -0500

    added commons-io dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,10 +13,12 @@ grails.project.dependency.resolution = {
     dependencies {
         compile 'com.rabbitmq:amqp-client:3.1.3'
         compile 'net.jodah:lyra:0.4.3'
+	compile 'commons-io:commons-io:2.2'
     }
 
     plugins {
-        build ":release:2.2.1", {
+        build(":release:3.0.1",
+              ":rest-client-builder:1.0.3") {
             export = false
         }
     }
